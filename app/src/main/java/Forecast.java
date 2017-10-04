f5b257efb1fe499c1c717139025f9fb5c956abea
chance <chancemagno@gmail.com>
2017-10-04 08:22:59 -0400

    create forecast model

---
+++ app/src/main/java/Forecast.java
@@ -0,0 +1,65 @@
+
+
+public class Forecast {
+    private String mCityName;
+    private String mHumidity;
+    private String mPressure;
+    private String mIcon;
+    private String mCurrentTemp;
+    private String mMaxTemp;
+    private String mMinTemp;
+    private String mCloudPercentage;
+
+    public Forecast(
+            String mCityName,
+            String mHumidity,
+            String mPressure,
+            String mIcon,
+            String mCurrentTemp,
+            String mMaxTemp,
+            String mMinTemp,
+            String mCloudPercentage) {
+
+        this.mCityName = mCityName;
+        this.mHumidity = mHumidity;
+        this.mPressure = mPressure;
+        this.mIcon = mIcon;
+        this.mCurrentTemp = mCurrentTemp;
+        this.mMaxTemp = mMaxTemp;
+        this.mMinTemp = mMinTemp;
+        this.mCloudPercentage = mCloudPercentage;
+    }
+
+    public String getCityName() {
+        return mCityName;
+    }
+
+    public String getHumidity() {
+        return mHumidity;
+    }
+
+    public String getPressure() {
+        return mPressure;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public String getCurrentTemp() {
+        return mCurrentTemp;
+    }
+
+    public String getMaxTemp() {
+        return mMaxTemp;
+    }
+
+    public String getMinTemp() {
+        return mMinTemp;
+    }
+
+    public String getCloudPercentage() {
+        return mCloudPercentage;
+    }
+}
+

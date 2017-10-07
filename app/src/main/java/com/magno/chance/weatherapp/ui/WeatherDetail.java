d01f592d36f520dfcda72c732a48b5b01370a2cb
chance <chancemagno@gmail.com>
2017-10-07 09:26:02 -0700

    add logic to unwrap forecast in weatherdetail activity

--- app/src/main/java/com/magno/chance/weatherapp/ui/WeatherDetail.java
+++ app/src/main/java/com/magno/chance/weatherapp/ui/WeatherDetail.java
@@ -2,16 +2,21 @@ package com.magno.chance.weatherapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.magno.chance.weatherapp.R;
+import com.magno.chance.weatherapp.models.Forecast;
+
+import org.parceler.Parcels;
 
 public class WeatherDetail extends AppCompatActivity {
-
+    private Forecast mForecast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_detail);
 
+        mForecast = Parcels.unwrap(getIntent().getParcelableExtra("forecast"));
 
     }
 }

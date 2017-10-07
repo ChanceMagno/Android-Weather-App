982741de0b59d8419b40f6d0d399b6e5c06eb7d9
chance <chancemagno@gmail.com>
2017-10-07 12:43:05 -0700

    add butterknife dependencies, bind text view to see city in weather detail activity

--- app/src/main/java/com/magno/chance/weatherapp/ui/WeatherDetail.java
+++ app/src/main/java/com/magno/chance/weatherapp/ui/WeatherDetail.java
@@ -1,22 +1,47 @@
 package com.magno.chance.weatherapp.ui;
 
+import android.content.SharedPreferences;
+import android.support.annotation.BinderThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
+import com.magno.chance.weatherapp.Constants;
 import com.magno.chance.weatherapp.R;
 import com.magno.chance.weatherapp.models.Forecast;
 
 import org.parceler.Parcels;
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class WeatherDetail extends AppCompatActivity {
     private Forecast mForecast;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    @BindView(R.id.cityNameTextView) TextView mCityNameTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_detail);
+        ButterKnife.bind(this);
+
+
 
         mForecast = Parcels.unwrap(getIntent().getParcelableExtra("forecast"));
 
+        setPageContent();
+
     }
+
+
+    public void setPageContent(){
+        mCityNameTextView.setText(mForecast.getCityName());
+    }
+
 }
